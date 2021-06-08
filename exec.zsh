d0389c46db1a8cfd43018627d57942ee1a2e14f1
ntoshihi <ntoshihi@student.42tokyo.jp>
2021-06-08 16:43:20 +0900

    re-name

--- exec.zsh
+++
@@ -1,1 +0,0 @@
-seq 1 1000 | gshuf | head -n 100 | xargs echo | xargs ./a.out
--- test.zsh
+++ test.zsh
@@ -1,1 +1,2 @@
 ls srcs/*.c libft/*.c | tr '\n' ' ' | xargs gcc -g test.c
+seq 1 1000 | gshuf | head -n 100 | xargs echo | xargs ./a.out
