581d01c88dd336f2f0f0c5477698da3c7477a506
ntoshihi <ntoshihi@student.42tokyo.jp>
2021-12-04 21:11:29 +0900

    made approx_sqrt

--- sources/math_utils.c
+++ sources/math_utils.c
@@ -15,3 +15,21 @@ int	max(int a, int b)
 	else
 		return (a);
 }
+
+int	approx_sqrt(int n)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (i * i < n)
+	{
+		j = i;
+		i++;
+	}
+	if (2 * n < i * i + j * j)
+		return (j);
+	else
+		return (i);
+}
